67cd5bffc303ce3f166aa9b3f0af4926f0de418b
angel23v <angelvazquezvirgen@gmail.com>
2024-03-12 22:24:30 -0600

    Google Social Login
    Setting up files to be able to access at the app by Google

--- Gemfile
+++ Gemfile
@@ -77,3 +77,10 @@ group :test do
 end
 
 gem 'image_processing', '>= 1.2'
+
+gem 'dotenv', groups: %i[development test]
+
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection', '~> 1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'main#home'
 
+  devise_for :users,
+             controllers: { registrations: 'users/registrations', sessions: 'users/sessions',
+                            omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users
   resources :posts
 end
