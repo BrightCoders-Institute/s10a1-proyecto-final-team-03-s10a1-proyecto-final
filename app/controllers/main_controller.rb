4a0c609b3fc10f9204f6c54ca588a11dfefb063d
Alan Hernández <ahernandez102@ucol.mx>
2024-03-20 09:48:40 -0600

    Reacting a post created #33

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
   before_action :authenticate_user!
   def home
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+    @user_likes = current_user.likes.where(post_id: @posts.map(&:id)).index_by(&:post_id)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,16 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def new
+    @like = Like.new
+  end
+
+  def increment_post_likes
+    post.increment!(:likes_count)
+  end
+
+  def decrement_post_likes
+    post.decrement!(:likes_count)
+  end
 end
