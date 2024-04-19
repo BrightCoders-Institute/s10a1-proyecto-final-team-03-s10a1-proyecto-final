6cd753fbebabceef908f1bebaa2476d89d4f85f8
Alejandro <alexavdstiles@gmail.com>
2024-04-18 19:49:13 -0600

    Re-style user profile view

--- app/javascript/controllers/profile_controller.js
+++ app/javascript/controllers/profile_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  removeActive() {
+    const profileSections = document.querySelector(".profile-sections");
+    const active = profileSections.querySelector(".active");
+    active.classList.remove("active");
+  }
+  
   connect() {
     let video = document.querySelector(".video");
     let post = document.querySelector(".post");
@@ -10,18 +16,24 @@ export default class extends Controller {
 
     video.addEventListener("click", (e) => {
       e.preventDefault();
+      this.removeActive();
+      video.classList.add("active");
       posts.classList.add("hidden");
       images.classList.add("hidden");
     });
 
     image.addEventListener("click", (e) => {
       e.preventDefault();
+      this.removeActive();
+      image.classList.add("active");
       posts.classList.add("hidden");
       images.classList.remove("hidden");
     });
 
     post.addEventListener("click", (e) => {
       e.preventDefault()
+      this.removeActive();
+      post.classList.add("active");
       posts.classList.remove("hidden");
       images.classList.add("hidden");
     });
