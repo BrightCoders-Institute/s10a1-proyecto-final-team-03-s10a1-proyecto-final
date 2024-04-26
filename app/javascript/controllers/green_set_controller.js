8ced0ce0ccf92acd97e1669cb6e5c737e62b985a
etormasa <hector.meza.141@gmail.com>
2024-04-26 17:48:36 -0600

    Change class names fix bugs
    fix bug in js, when edit a set, checkbox does not changed div color to
    green

--- app/javascript/controllers/green_set_controller.js
+++ app/javascript/controllers/green_set_controller.js
@@ -2,18 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    document.addEventListener('DOMContentLoaded', function() {
-        const checkboxes = document.querySelectorAll('.miCheckbox input[type="checkbox"]');
-        const divs = document.querySelectorAll('.serie_content');
+    const checkboxes = document.querySelectorAll('.setsCheckbox input[type="checkbox"]');
+    const divs = document.querySelectorAll('.serie_content');
 
-        checkboxes.forEach((checkbox, index) => {
-            checkbox.addEventListener('change', function() {
-                if (this.checked) {
-                    divs[index].classList.add('green');
-                } else {
-                    divs[index].classList.remove('green');
-                }
-            });
+    checkboxes.forEach((checkbox, index) => {
+        checkbox.addEventListener('change', function() {
+            if (this.checked) {
+                divs[index].classList.add('green');
+            } else {
+                divs[index].classList.remove('green');
+            }
         });
     });
   }
