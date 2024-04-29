9141c867511dc081cf41daa1e041e2b08d113581
angel23v <angelvazquezvirgen@gmail.com>
2024-04-28 19:55:52 -0600

    Target use
    Replace the way to get DOM elements this for the purpose of Stimulus

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["search_input", "searched"];
+
   async connect() {
     const response = await fetch("/search/new");
     const data = await response.json();
@@ -17,10 +19,10 @@ export default class extends Controller {
       ];
     });
 
-    let input = document.querySelector(".search_input");
-    let content_searched = document.querySelector(".searched");
+    // let input = document.querySelector(".search_input");
+    // let content_searched = document.querySelector(".searched");
 
-    input.addEventListener("input", (e) => {
+    this.search_inputTarget.addEventListener("input", (e) => {
       e.preventDefault();
 
       let input_search = e.target.value;
@@ -36,12 +38,12 @@ export default class extends Controller {
       const content = [...filteredPost, ...filteredUser];
 
       content
-        ? (content_searched.innerHTML = content.map((element) =>
+        ? (this.searchedTarget.innerHTML = content.map((element) =>
             element.length > 2
               ? `<a href=/posts/${element[0]}>${element[1]}`
               : `<a href=/users/${element[0]}>${element[1]}</a>`
           ))
-        : (content_searched.innerHTML = `<p>User or Post does not exist</p>`);
+        : (this.searchedTarget.innerHTML = `<p>User or Post does not exist</p>`);
     });
   }
 }
