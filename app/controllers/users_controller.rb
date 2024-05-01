83b7475f31ff232234d7ef09462b5cb865baf191
Alan Hernández <ahernandez102@ucol.mx>
2024-04-30 23:08:39 -0600

    Implement Direct Messages #80

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @follow = User.where(id: current_user.id)
-    @posts = @user.posts.order(created_at: :desc)
+    load_users
+    load_social_actions
+  end
 
-    # Filtramos los "me gusta" del usuario actual para las publicaciones del usuario en cuestión
-    @user_likes = @user.likes.where(post_id: @posts.pluck(:id)).index_by(&:post_id)
+  def chat
+    load_users
+    @message = Message.new
+    @messages = load_user_messages(@user, current_user)
+    render 'chats/index'
   end
 
   def new
     @user = User.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -35,5 +37,29 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :lastName, :birthday, :weight, :height, :email, :image_profile)
+  end
+
+  def load_users
+    @user = User.find(params[:id])
+    @users = User.all_except(current_user)
+  end
+
+  def load_social_actions
+    @follow = User.where(id: current_user.id)
+    @posts = @user.posts.order(created_at: :desc)
+
+    # Filtramos los "me gusta" del usuario actual para las publicaciones del usuario en cuestión
+    @user_likes = current_user.likes.where(post_id: @posts.map(&:id)).index_by(&:post_id)
+  end
+
+  def load_user_messages(user1, user2)
+    @room_name = get_name(user1, user2)
+    @single_room = Room.where(name: @room_name).first || Room.create_private_room([user1, user2], @room_name)
+    @single_room.messages.order(created_at: :asc)
+  end
+
+  def get_name(user1, user2)
+    user = [user1, user2].sort
+    "private_#{user[0].id}_#{user[1].id}"
   end
 end
--- db/migrate/20240423124914_create_streaks.rb
+++ db/migrate/20240423124914_create_streaks.rb
@@ -1,7 +1,7 @@
 class CreateStreaks < ActiveRecord::Migration[7.1]
   def change
     create_table :streaks do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
       t.integer :current_streak
       t.integer :longest_streak
       t.date :last_login_date
